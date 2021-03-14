8e60dd9704babde6a8bb096de2472081367f7672
Valeriy Semenkov <unfairlly@gmail.com>
2021-03-14 07:29:24 +0300

    123

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        int start_balance = 100;
+        int payment = 1100;
+        int bonus = 0;
+        if (payment >= 1099) {
+            bonus = payment / 100;
+
+        }
+        int balance = payment + start_balance + bonus;
+        System.out.println(balance);
+        System.out.println(bonus);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
